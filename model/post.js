7a4ec7636145a2f4827916b1320f9445323e5710
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-12-10 19:37:20 -0500

    added default date for timestamp

--- model/post.js
+++ model/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   title: { type: String },
   content: { type: String },
-  timeStamp: { type: Date },
+  timeStamp: { type: Date, default: Date.now },
   isPublic: { type: Boolean },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }
 });

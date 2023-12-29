0a2eb7499640323ef599ea0dd62ebb2c0a6f1313
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-12-29 00:36:22 -0500

    added: name of the api for the url request on the begin

--- routes/blog.js
+++ routes/blog.js
@@ -8,20 +8,27 @@ const replyController = require("../controller/replyController");
 const auth = require("../middleware/auth");
 
 
+// get all users
+router.get("/blog-api/users/allUsers", userController.get_users);
+
 // register
-router.post("/register", userController.create_user_post);
+router.post("/blog-api/register", userController.create_user_post);
 
 //login
-router.post("/login", userController.login_user_post);
+router.post("/blog-api/login", userController.login_user_post);
 
 //create post 
-router.post("/new-post", auth, postController.create_post);
+router.post("/blog-api/new-post", auth, postController.create_post);
 
 // create comment
-router.post("/posts/:id/new-comment", auth, commentController.create_comment);
+router.post("/blog-api/posts/:id/new-comment", auth, commentController.create_comment);
+
+
+// get all replies 
+router.get("/blog-api/replies/allReplies", replyController.get_all_replies);
 
 //create a comment reply 
-router.post("/comments/:id/new-reply", auth, replyController.new_reply);
+router.post("/blog-api/comments/:id/new-reply", auth, replyController.new_reply);
 
 // auth
 router.post("/welcome", auth, (req, res) => {

0c60e6fa59c14112c26bdb1ed5080f24bc0182a7
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2023-12-30 00:27:50 -0500

    handle get all posts

--- controller/postController.js
+++ controller/postController.js
@@ -1,5 +1,23 @@
 const Post = require("../model/post");
 
+// get all posts
+exports.get_all_post = async (req, res, next) => {
+  try {
+    // get the post
+    const posts = await Post.find()
+    
+    if(!posts) {
+      res.status(400).send("Posts not found")
+    }
+
+    res.status(200).send(posts)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({error: message})
+  }
+}
+
 
 exports.create_post = async (req, res, next) => {
 

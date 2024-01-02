3559b5cc70dbe0d8a64e94e54ff7cdcb367a4cdd
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-02 17:31:06 -0500

    handle delete one comment added

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,5 +1,45 @@
 const Comment = require("../model/comment");
+const Post = require("../model/post");
 
+// handle delete comment
+exports.delete_one_comment = async(req, res, next) => {
+  try {
+    
+    const commentId = req.params.commentId;
+    const postId = req.params.postId;
+
+    if(!commentId || !postId) {
+      res.status(400).json({error :"Comment  ID or postId not provided"})
+    }
+
+    const comment = await Comment.findById(commentId)
+    const post = await Post.findById(postId)
+
+
+    if(!comment) {
+      res.status(404).json({error: "Comment not found"})
+    }
+
+    if(!post) {
+      res.status(404).json({error: "Post not found"})
+    }
+
+    if(
+      req.user.user_id == comment.user ||
+      req.user.user_id == post.user
+    ) {
+
+      await Comment.findByIdAndDelete(commentId)
+      return res.status(200).json({message: "Comment deleted successfully"})
+    } else {
+      return res.status(403).json({error: "Unauthorized to delete comment"})
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: error.message})
+  }
+}
 
 exports.get_all_comments = async (req, res, next) => {
   try {

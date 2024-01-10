bb35b12b008f8436909904dce004956b6f34d005
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-10 15:26:19 -0500

    handle get one post by id

--- controller/postController.js
+++ controller/postController.js
@@ -48,6 +48,26 @@ exports.get_all_post = async (req, res, next) => {
   }
 }
 
+// get one post 
+
+exports.get_one_post = async (req, res, next) => {
+  try {
+    const postId = req.params.id
+    const post = await Post.findById(postId)
+
+    if(!post) {
+      res.status(404).send("Post not found")
+      return
+    }
+
+    res.status(200).send(post)
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({error: error.message})
+  }
+}
+
 
 exports.create_post = async (req, res, next) => {
 

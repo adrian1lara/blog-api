169c9ad6ddf7027108bb481688f1f1ef7eec1162
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-13 15:33:14 -0500

    handle virtuals, and logic of latest time

--- model/post.js
+++ model/post.js
@@ -1,3 +1,5 @@
+
+const { DateTime } = require("luxon");
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
@@ -6,6 +8,33 @@ const PostSchema = new mongoose.Schema({
   timeStamp: { type: Date, default: Date.now },
   isPublic: { type: Boolean },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }
+  
+}, 
+{
+  toJSON: { virtuals: true } // Set the toJSON option for virtuals
+});
+
+PostSchema.virtual("latest").get(function() {
+  const currentDateTime = DateTime.now();
+  const postDateTime = DateTime.fromJSDate(this.timeStamp);
+
+  const diff = currentDateTime.diff(postDateTime);
+
+  // get the diff of seconds, minutes, hours, and days.
+  const seconds = Math.floor(diff.as("seconds"));
+  const minutes = Math.floor(diff.as("minutes"));
+  const hours = Math.floor(diff.as("hours"));
+  const days = Math.floor(diff.as("days"));
+
+  if (seconds < 60) {
+    return `${seconds} seconds`;
+  } else if (minutes < 60) {
+    return `${minutes} minutes`;
+  } else if (hours < 24) {
+    return `${hours} hours`;
+  } else {
+    return `${days} days`;
+  }
 });
 
 module.exports = mongoose.model("post", PostSchema);
